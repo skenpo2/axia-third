caf37f2fbdc3ca57808bea5f73a2ca9d9b0593c3
Micheal <michealobarewon2015@gmail.com>
2025-03-01 00:04:58 +0100

    login limiter

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const authController = require('../controllers/auth.controllers');
-// const loginLimiter = require('../middlewares/errorHandler');
+const loginLimiter = require('../middlewares/loginLimiter');
 
 const routes = express.Router();
 
-routes.post('/auth', authController.login);
+routes.post('/auth', loginLimiter, authController.login);
 routes.get('/auth/refresh', authController.refresh);
 routes.post('/auth/logout', authController.logout);
 
